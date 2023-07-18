604f8a875b9d2be39ca0d71cac0da7c39238fc61
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-18 17:41:51 +0800

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // Check if arguments given is a negative number, or an invalid data type. If so, return "ERROR"
+    if (start < 0 || end < 0 || typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    // Check if first argument is larger then second. If so, swap the 2 numbers
+    if (start > end) {
+        let number = start;
+        start = end;
+        end = number;
+    }
+
+    // Loop through all numbers between start and end, including end
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
