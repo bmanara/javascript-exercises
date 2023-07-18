5111fc1c103876b7604df0d31c93721c75f803c0
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-18 17:22:36 +0800

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arrayString = string.split("");
+    arrayString = arrayString.reverse();
 
+    let reversedString = arrayString.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
