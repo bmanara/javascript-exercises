15e2ed6eccb52119a8810907428f7e9c11d2f4b0
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-19 10:36:17 +0800

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * 5 / 9;
+  if (celsius % 1 != 0) {
+    return parseFloat(celsius.toFixed(1));
+  }
+
+  return celsius;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = (celsius * 9 / 5) + 32;
+  if (farenheit % 1 != 0) {
+    return parseFloat(farenheit.toFixed(1));
+  }
+  return farenheit;
 };
 
 // Do not edit below this line
