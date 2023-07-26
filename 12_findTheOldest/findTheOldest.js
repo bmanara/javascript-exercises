df3caef12f25855ea8f47e034de5f99793a0c96b
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-26 16:38:41 +0800

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
 
+    people.forEach(person => {
+        if ("yearOfDeath" in person) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            person.age = date.getFullYear() - person.yearOfBirth;
+        }
+    })
+
+    people.sort((a, b) => b.age - a.age)
+    return people[0];
 };
 
 // Do not edit below this line
