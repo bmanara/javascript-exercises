7684e14306c8ba4d4d61f5d8b8476a638a6055fd
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-26 15:37:20 +0800

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = string.toLowerCase().split("");
+    arr = arr.filter((letter) => {
+        return /^[a-z0-9]$/.test(letter);
+    })
 
+    let arr2 = [...arr].reverse();
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+
+    // Second try after seeing solution,
+    // let processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
