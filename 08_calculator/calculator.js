4514d91aa3812ef5cb807d460e8f92581971258e
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-26 15:11:08 +0800

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
 const multiply = function() {
-
+  let numbers = Array.from(arguments);
+  return numbers.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) return 1;
+  
+  // Recursion
+  return factorial(a - 1) * a;
 };
 
 // Do not edit below this line
