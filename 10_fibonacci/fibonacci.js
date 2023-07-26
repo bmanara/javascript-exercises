41f7dfc4c432357c13b9e95102d692bcba21bdb9
bmanara <65152569+bmanara@users.noreply.github.com>
2023-07-26 15:51:14 +0800

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    position = +position;
+    if (position < 0) {
+        return "OOPS";
+    }
+    else if (position == 1 || position == 2) {
+        return 1;
+    }
 
+    return fibonacci(position - 1) + fibonacci(position - 2);
 };
 
 // Do not edit below this line
